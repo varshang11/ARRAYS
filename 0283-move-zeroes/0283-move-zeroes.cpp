10070a660ea05bf09308c2c961e43e563b95411d
Varsha N G <varsha.ng2006@gmail.com>
2026-07-24 10:09:30 +0530

    Time: 0 ms (100%), Space: 24.7 MB (5.03%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> temp;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0)
+                temp.push_back(nums[i]);
+        }
+        int nz=temp.size();
+        for(int i=0;i<nz;i++){
+            nums[i]=temp[i];
+        }
+        for(int i=nz;i<n;i++){
+            nums[i]=0;
+        }
+
+        // int index=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]!=0){
+        //         swap(nums[index],nums[i]);
+        //         index++;
+        //     }
+        // }
+    }
+};
